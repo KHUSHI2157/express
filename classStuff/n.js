f2c4fc93fc83c96023c282015cb641de42fc5e6b
LOVENISH87 <150184006+LOVENISH87@users.noreply.github.com>
2025-11-12 16:11:02 +0530

    nothing

--- classStuff/n.js
+++ classStuff/n.js
@@ -1,1 +1,32 @@
-console.log("some shyt");
+import express from 'express';
+import { body, validationResult } from 'express-validator';
+// import isEmail from '../node_modules/validator/es/lib/isEmail';
+// import isLength from '../node_modules/validator/es/lib/isLength';
+
+const app = express();
+
+app.use(express.json())
+
+app.get('/' , (req, resp) => {
+    resp.send("hey there!!!")
+})
+
+app.post('/reg',
+
+    [
+        // rule
+        body("name").notEmpty().withMessage("name nhi hai bhdve!!"),
+        body("email").notEmpty().isEmail().withMessage("email!!"),
+        body('password').isLength({ min: 8 }).withMessage("pass is required!!!")
+    ],
+    (req, resp) => {
+        // resp.json({ message: "trash" });
+        const error = validationResult(req)
+        if(!error.isEmpty()){
+            return resp.json(error);
+        }
+        else {resp.send('regsca  successful')}
+    }
+)
+
+app.listen(4800)
